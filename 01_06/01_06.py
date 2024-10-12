af869ef9595ead8a786429d557e2e7246966d4d9
lincgr <lincgr@gmail.com>
2024-10-12 11:12:08 +0200

    01_06 Operaciones aritméticas sobre tensores en PyTorch

---
+++ 01_06/01_06.py
@@ -0,0 +1,9 @@
+import torch
+
+tensor_a = torch.tensor([[1, 2, 3], 
+                         [4, 5, 6], 
+                         [7, 8, 9]], dtype=torch.float32)
+
+tensor_b = torch.tensor([[9, 8, 7], 
+                         [6, 5, 4], 
+                         [3, 2, 1]], dtype=torch.float32)
